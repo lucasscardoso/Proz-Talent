3fa39f03e5f8d89afc1d5e04bc849fc707f5bb00
gajeel redfox <lucas.silva.cardoso1996@gmail.com>
2024-06-27 20:11:47 -0300

    exercicio sobre criação e utilização de vetores

---
+++ exibeProdutos_exibeAnoNascimento.py
@@ -0,0 +1,8 @@
+produtos_loja_comida = ["Pão", "Leite", "Queijo", "Maçã", "Café"]
+
+
+anos_nascimento_familia_amigos = [1980, 1992, 1995, 2000, 2005]
+
+
+print("Produtos da loja de comida:", produtos_loja_comida)
+print("Anos de nascimento de familiares e amigos:", anos_nascimento_familia_amigos)
