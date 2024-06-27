61bd768d18c57ef708bbc31c0b40736bfab7dddd
gajeel redfox <lucas.silva.cardoso1996@gmail.com>
2024-06-27 20:15:53 -0300

    exercicio de lista de musicos

---
+++ lista_de_musicos.py
@@ -0,0 +1,14 @@
+lista_musicos = [
+    'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 
+    'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 
+    'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'
+]
+
+
+quantidade_elementos = len(lista_musicos)
+print("Quantidade de elementos:", quantidade_elementos)
+
+
+print("Dado salvo no índice 2:", lista_musicos[2])
+print("Dado salvo no índice 9:", lista_musicos[9])
+print("Dado salvo no índice 14:", lista_musicos[14])
